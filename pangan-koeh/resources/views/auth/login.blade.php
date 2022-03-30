b97a26149cd18037a8fa58e227644709bc0dddbf
mcekasp <cekas24@gmail.com>
2022-03-30 23:10:54 +0700

    nyicil login dan register

--- pangan-koeh/resources/views/auth/login.blade.php
+++ pangan-koeh/resources/views/auth/login.blade.php
@@ -1,13 +1,40 @@
 @extends('layouts.base')
 
 @section('content')
-    <div id="login-page">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col" id="background-auth">
+            <div class="col-xl-7" id="background-auth">
                 <h1>Hello Guys! ini buat yang ijo</h1>
             </div>
-            <div class="col">
-                <h1>ini buat form</h1>
+            <div class="col-xl-5">
+                <div class="form">
+                    <div class="greet">
+                        <h2><b>Hello!</b></h2>
+                        <p>Selamat Datang Kembali!</p>
+                    </div>
+                    <div class="formulir">
+                        <form action="">
+                            <div class="mb-3 input-group">
+                                <span class="input-group-text rounded-pill" id="basic-addon1"><i class="bi bi-envelope"></i></span>
+                                <input type="email" class="form-control rounded-pill" id="inputEmail" name="email" placeholder="E-mail" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="mb-3 input-group">
+                                <span class="input-group-text rounded-pill" id="basic-addon2"><i class="bi bi-lock-fill"></i></span>
+                                <input type="password" class="form-control rounded-pill" id="inputPassword" name="password" placeholder="Password" aria-describedby="basic-addon2">
+                            </div>
+                            <div class="mb-1">
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-success rounded-pill border border-light" type="button" style="background-color: #72DC58">Login</button>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <center>
+                                    <a href="{{ url('register') }}" style="font-size: 12px">Belum punya akun? Daftar sekarang!</a>
+                                </center>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- pangan-koeh/resources/views/auth/register.blade.php
+++ pangan-koeh/resources/views/auth/register.blade.php
@@ -1,13 +1,48 @@
 @extends('layouts.base')
 
 @section('content')
-    <div id="register-page">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col">
+            <div class="col-xl-7" id="background-auth">
                 <h1>Hello Guys! ini buat yang ijo</h1>
             </div>
-            <div class="col">
-                <h1>ini buat form</h1>
+            <div class="col-xl-5">
+                <div class="form">
+                    <div class="greet">
+                        <h2><b>Hello!</b></h2>
+                        <p>Daftar untuk memulai!</p>
+                    </div>
+                    <div class="formulir">
+                        <form action="">
+                            <div class="mb-3 input-group">
+                                <span class="input-group-text rounded-pill" id="basic-addon1"><i class="bi bi-person-fill"></i></span>
+                                <input type="text" class="form-control rounded-pill" id="inputUsername" name="username" placeholder="Username" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="mb-3 input-group">
+                                <span class="input-group-text rounded-pill" id="basic-addon2"><i class="bi bi-envelope"></i></span>
+                                <input type="email" class="form-control rounded-pill" id="inputEmail" name="email" placeholder="E-mail" aria-describedby="basic-addon2">
+                            </div>
+                            <div class="mb-3 input-group">
+                                <span class="input-group-text rounded-pill" id="basic-addon3"><i class="bi bi-lock-fill"></i></span>
+                                <input type="password" class="form-control rounded-pill" id="inputPassword" name="password" placeholder="Password" aria-describedby="basic-addon3">
+                            </div>
+                            <div class="mb-3 input-group">
+                                <span class="input-group-text rounded-pill" id="basic-addon2"><i class="bi bi-lock-fill"></i></span>
+                                <input type="password" class="form-control rounded-pill" id="cekPassword" name="cekPassword" placeholder="Konfirmasi Password" aria-describedby="basic-addon2">
+                            </div>
+                            <div class="mb-1">
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-success rounded-pill border border-light" type="button" style="background-color: #72DC58">Daftar</button>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <center>
+                                    <a href="{{ url('login') }}" style="font-size: 12px">Sudah punya akun? Login disini</a>
+                                </center>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
