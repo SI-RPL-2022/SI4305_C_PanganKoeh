6fad4623db2d7e1438109edf79caea7983059c26
muthiqonita <muthiqonita@gmail.com>
2022-04-04 00:47:00 +0700

    ehet

--- pangan-koeh/resources/views/layouts/navbar.blade.php
+++ pangan-koeh/resources/views/layouts/navbar.blade.php
@@ -17,14 +17,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="menu">
         <li class="nav-item">
-<<<<<<< HEAD
           <a class="nav-link" href="/">Beranda</a>
-=======
-          <a class="nav-link" href="{{ url('/') }}">Beranda</a>
->>>>>>> 816e4abbe8854377de06d4f9f63ad55afae6d82b
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Tentang Kami</a>
+          <a class="nav-link" href="#cardAbout">Tentang Kami</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Informasi</a>
