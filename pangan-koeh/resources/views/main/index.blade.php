685fecf46a956f0b8a52eae420102a17810e74c6
muthiqonita <muthiqonita@gmail.com>
2022-04-07 13:57:57 +0700

    index (harga rata2) dan informasi

--- pangan-koeh/resources/views/main/index.blade.php
+++ pangan-koeh/resources/views/main/index.blade.php
@@ -39,31 +39,150 @@
           <center class="py-3">
             <h1 style="color: #677A68"><b>Rata-rata Harga Pangan</b></h1>
           </center>
-          <div id="carouselExampleIndicators" class="carousel slide py-5" data-bs-ride="carousel">
+          <div id="carouselExample" class="carousel slide py-5" data-bs-ride="carousel">
             <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div class="carousel-inner" id="pangan">
               <div class="carousel-item active">
                 <div class="row">
-                  <div class="card px-3" style="width: 15rem;">
-                    <img src="frontend\gambar\Pangan\minyakgoreng.jpg" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\1.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
                     <div class="card-body">
-                      <h5 class="card-title">Contoh</h5>
-                      <p class="card-text">Nanti card ini di-looping</p>
-                      <a href="#" class="btn btn-success">Go somewhere</a>
+                      <h5 class="card-title" style="font-size:15px"><b>Minyak Goreng Sanco /liter</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\2.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Beras Pandan Wangi /kg</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 12.813,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\3.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Cabe Hijau Besar /ons</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 4.000,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\4.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Ayam Potong /kg</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 38.000,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\5.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Bawang Merah /ons</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 3.200,-</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="carousel-item">
+                <div class="row">
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\6.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Tomat /kg</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\7.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Gula Gulaku /kg</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\8.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Daging Sapi /kg</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\9.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Cabe Rawit /ons</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\10.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Paprika /kg</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="carousel-item">
+                <div class="row">
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\11.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Cabe Merah /ons</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\12.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Sayur Sawi Hijau /kg</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\13.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Sayur Kangkung /kg</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\14.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Bawang Bombay /kg</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
+                    </div>
+                  </div>
+
+                  <div align="center" class="card px-3 py-3" style="width: 12rem; margin-left:20px">
+                    <img src="frontend\gambar\Pangan\15.png" id="card-Beranda" class="card-img-top" alt="..." width="100%" height="120px">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size:15px"><b>Daun Seledri /ons</b></h5>
+                      <a href="#" class="btn"  style="background-color: #3CD3A6; color: white">Rp. 14.500,-</a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <button class="carousel-control-prev" id="left" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <button class="carousel-control-prev" id="left" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" id="right" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <button class="carousel-control-next" id="right" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="visually-hidden">Next</span>
             </button>
@@ -78,7 +197,7 @@
           <svg id="bulet" width="100" height="100" viewBox="0 0 352 344" fill="none" xmlns="http://www.w3.org/2000/svg">
             <ellipse cx="175.782" cy="171.75" rx="175.782" ry="171.75" fill="#3CD3A6"/>
           </svg>
-          <div class="card text-center py-4 px-3" id="cardAbout">
+          <div class="card text-center py-4 px-3 shadow-lg p-3 mb-5 bg-white rounded-0 justify-content-center" id="cardAbout">
             <div class="card-body">
               <p class="card-text py-2">Pangan-Koeh memberikan layanan informasi-informasi mengenai komodtitas pangan, berupa perbandingan harga komoditas pangan antar pasar, harga rata-rata komoditas pangan pada setiap pasar, dan harga tertinggi dan terendah dari setiap komoditas pangan.</p>
               <p class="card-text"><b>Kelompok 3</b></p>
--- pangan-koeh/resources/views/main/informasi.blade.php
+++ pangan-koeh/resources/views/main/informasi.blade.php
@@ -12,18 +12,18 @@
                     </center>
                 </div>
                 <div class="body-page">
-                    <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="row row-cols-1 row-cols-md-2 g-1">
                         <div class="col">
                             <div class="card mb-3" style="max-width: 540px;">
                                 <div class="row g-0">
                                   <div class="col-md-4">
-                                    <img src="frontend\gambar\Pangan\meme-minyak-goreng.jpg" id="card-informasi" class="img-fluid rounded-start" alt="...">
+                                    <img src="frontend\gambar\Informasi\1.png" style="height: 220px" id="card-informasi" class="img-fluid rounded-start" alt="...">
                                   </div>
                                   <div class="col-md-8">
                                     <div class="card-body">
-                                      <h5 class="card-title"><b>Langkanya Minyak Goreng</b></h5>
-                                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                      <a href="#">Learn More<i class="bi bi-arrow-right"></i></a>
+                                      <h5 class="card-title" style="font-size: 17px"><b>Kenapa Minyak Goreng Mahal dan Langka? Ini Penyebabnya Menurut Kemendag</b></h5>
+                                      <p class="card-text" style="font-size: 13px">Kenaikan harga minyak goreng telah terjadi sejak akhir 2021 dan sampai saat ini belum terselesaikan. Dimulai sejak November 2021, harga minyak goreng ...</p>
+                                      <a href="#" style="font-size: 14px">Learn More<i class="bi bi-arrow-right"></i></a>
                                     </div>
                                   </div>
                                 </div>
@@ -33,13 +33,13 @@
                             <div class="card mb-3" style="max-width: 540px;">
                                 <div class="row g-0">
                                   <div class="col-md-4">
-                                    <img src="frontend\gambar\Pangan\meme-minyak-goreng.jpg" id="card-informasi" class="img-fluid rounded-start" alt="...">
+                                    <img src="\frontend\gambar\Informasi\2.png" style="height: 220px" id="card-informasi" class="img-fluid rounded-start" alt="...">
                                   </div>
                                   <div class="col-md-8">
                                     <div class="card-body">
-                                      <h5 class="card-title"><b>Langkanya Minyak Goreng</b></h5>
-                                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                      <a href="#">Learn More<i class="bi bi-arrow-right"></i></a>
+                                      <h5 class="card-title" style="font-size: 17px"><b>Kenaikan Harga Pangan Jauh Sebelum Lebaran, Asosiasi Pedagang: Tak Wajar</b></h5>
+                                      <p class="card-text" style="font-size: 13px">Sekretaris Jenderal Dewan Pimpinan Pusat Ikatan Pedagang Pasar Indonesia (Ikappi) Reynaldi Sarijowan menilai reli kenaikan harga kebutuhan pokok pada awal tahun ini tidak wajar ...</p>
+                                      <a href="#" style="font-size: 14px">Learn More<i class="bi bi-arrow-right"></i></a>
                                     </div>
                                   </div>
                                 </div>
@@ -49,13 +49,13 @@
                             <div class="card mb-3" style="max-width: 540px;">
                                 <div class="row g-0">
                                   <div class="col-md-4">
-                                    <img src="frontend\gambar\Pangan\meme-minyak-goreng.jpg" id="card-informasi" class="img-fluid rounded-start" alt="...">
+                                    <img src="\frontend\gambar\Informasi\3.png" style="height: 220px" id="card-informasi" class="img-fluid rounded-start" alt="...">
                                   </div>
                                   <div class="col-md-8">
                                     <div class="card-body">
-                                      <h5 class="card-title"><b>Langkanya Minyak Goreng</b></h5>
-                                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                      <a href="#">Learn More<i class="bi bi-arrow-right"></i></a>
+                                      <h5 class="card-title" style="font-size: 17px"><b>Masyarakat Rentan di Pusaran Kenaikan Harga Pangan</b></h5>
+                                      <p class="card-text" style="font-size: 13px">Di tengah situasi pandemi covid-19 yang tak kunjung usai, daya tahan masyarakat menghadapi tekanan baru kenaikan harga pangan yang terus melambung diperkirakan tidak lagi kenyal. ...</p>
+                                      <a href="#" style="font-size: 14px">Learn More<i class="bi bi-arrow-right"></i></a>
                                     </div>
                                   </div>
                                 </div>
@@ -65,13 +65,13 @@
                             <div class="card mb-3" style="max-width: 540px;">
                                 <div class="row g-0">
                                   <div class="col-md-4">
-                                    <img src="frontend\gambar\Pangan\meme-minyak-goreng.jpg" id="card-informasi" class="img-fluid rounded-start" alt="...">
+                                    <img src="\frontend\gambar\Informasi\4.png" style="height: 220px" id="card-informasi" class="img-fluid rounded-start" alt="...">
                                   </div>
                                   <div class="col-md-8">
                                     <div class="card-body">
-                                      <h5 class="card-title"><b>Langkanya Minyak Goreng</b></h5>
-                                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                      <a href="#">Learn More<i class="bi bi-arrow-right"></i></a>
+                                      <h5 class="card-title" style="font-size: 17px"><b>Harga Minyak Goreng Curah Naik Lagi, Tembus Rp17.100 per Liter</b></h5>
+                                      <p class="card-text" style="font-size: 13px">Kenaikan indeks harga pangan dunia turut didorong pergerakan harga minyak nabati dan susu. Selain itu, harga sereal dan daging juga mengalami peningkatan yang signifikan. ...</p>
+                                      <a href="#" style="font-size: 14px">Learn More<i class="bi bi-arrow-right"></i></a>
                                     </div>
                                   </div>
                                 </div>
