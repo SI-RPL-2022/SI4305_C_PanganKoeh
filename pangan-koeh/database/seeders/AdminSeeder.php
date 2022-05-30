5afab1421258923e0f7f87fed37d69c2a35f0ff5
redittaaviyolla <reditaviyolla@gmail.com>
2022-05-30 21:08:04 +0700

    File baru

--- pangan-koeh/database/seeders/AdminSeeder.php
+++ pangan-koeh/database/seeders/AdminSeeder.php
@@ -192,5 +192,16 @@ class AdminSeeder extends Seeder
                 'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
             ]
         ]);
+
+        \App\Models\Price::insert([
+            [
+                'id_pasar' => '2',
+                'id_komoditas' => '3',
+                'harga' => '100000',
+                'tanggal' => '2022-05-12',
+                'kontributor' => 'BARA',
+                'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+            ]
+        ]);
     }
 }
