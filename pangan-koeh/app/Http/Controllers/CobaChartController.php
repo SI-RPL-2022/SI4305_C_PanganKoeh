a14823918899d9966e684f4b9c3cfb62f7491ddd
Barajati12 <barajati@student.telkomuniversity.ac.id>
2022-05-31 12:32:04 +0700

    [Done: Grafik #1]

--- pangan-koeh/app/Http/Controllers/CobaChartController.php
+++ pangan-koeh/app/Http/Controllers/CobaChartController.php
@@ -28,17 +28,17 @@ class CobaChartController extends Controller
     public function linechart($id_pangan, $id_pasar)
     {
         $result = DB::select(DB::raw("SELECT id_pasar,id_komoditas,harga,tanggal FROM `prices` WHERE id_komoditas = $id_pangan AND id_pasar= $id_pasar group by tanggal ORDER BY `prices`.`tanggal` ASC"));
-        // $data = "";
-        // foreach ($result as $val) {
-        //     $data .= "[new Date('" . $val->tanggal . "'), " . $val->harga . "],";
-        // }
-        // // dd($data);
-        // $chartData = $data;
+        $data = "";
+        foreach ($result as $val) {
+            $data .= "[new Date('" . $val->tanggal . "'), " . $val->harga . "],";
+        }
+        // dd($data);
+        $chartData = $data;
         return view('main.Dashboard1', [
             'pasar' => Market::find($id_pasar),
             'pangan' => Pangan::all(),
             'panganspes' => Pangan::find($id_pangan),
-            'grafik' => $result
+            'grafik' => $chartData
         ]);
     }
 }
--- pangan-koeh/resources/views/main/Dashboard1.blade.php
+++ pangan-koeh/resources/views/main/Dashboard1.blade.php
@@ -17,26 +17,15 @@ Pangankoeh
             data.addColumn('number', '{{ $panganspes->name }}');
 
             data.addRows([
-                [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-                [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-                [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-                [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-                [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-                [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-                [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-                [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-                [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-                [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-                [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-                [66, 70], [67, 72], [68, 75], [69, 80]
+                <?php echo $grafik;?>
             ]);
 
             var options = {
                 hAxis: {
-                title: 'Time'
+                title: 'Tanggal'
                 },
                 vAxis: {
-                title: 'Popularity'
+                title: 'Harga'
                 }
             };
 
@@ -55,9 +44,9 @@ Pangankoeh
 @include('layouts.navbar')
 @endif
 
-// {{-- @php
-//         dd($grafik);
-//     @endphp --}}
+{{-- @php
+    dd($grafik);
+@endphp --}}
 <div class="beranda">
     <div class="container" style="padding-top: 80px; padding-bottom: 80px">
         <div class="row justify-content-around">
@@ -101,7 +90,7 @@ Pangankoeh
                         <div class="row row-cols-4">
                             @foreach ($pangan as $item)
                             <div class="col pb-4">
-                                <a href="/hargaPangan1/{{ $item->id }}" type="button" class="btn btn-success rounded-pill py-2">{{ $item->name }}</a>
+                                <a href="/hargaPangan1/{{ $item->id }}/{{ $pasar->id }}" type="button" class="btn btn-success rounded-pill py-2">{{ $item->name }}</a>
                             </div>
                             @endforeach
                         </div>
