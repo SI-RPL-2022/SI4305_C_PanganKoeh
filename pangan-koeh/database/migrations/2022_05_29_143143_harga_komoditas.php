e796071732f5ef65c47a67a4f9840104aa7074bc
Barajati12 <barajati@student.telkomuniversity.ac.id>
2022-06-30 13:25:59 +0700

    wa

--- pangan-koeh/database/migrations/2022_05_29_143143_harga_komoditas.php
+++ pangan-koeh/database/migrations/2022_05_29_143143_harga_komoditas.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->string('harga');
             $table->string('tanggal');
             $table->string('kontributor');
-            $table->string('status');
+            $table->string('status')->nullable();
             $table->string('gambarpasar')->nullable();
             $table->rememberToken();
             $table->timestamps();
